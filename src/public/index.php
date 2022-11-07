118aa29805f24483a8701107650663af06c89582
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-11-07 21:51:58 +0900

    6. 構成を考えてみよう課題１

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,16 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!DOCTYPE html>
+<link rel="stylesheet" href="./index.css">
+<!-- 同じフォルダ内　./ 　-->
+<!--  -->
+<div>
+  <div class="titleWrapper">
+    <h2 class="title">何故TECH QUESTなのか</h2>
+  </div>
+  <div class="textWrapper">
+    <p class="text">ドメイン駆動設計の考え方はPythonやJava、Goなど、あらゆる言語での開発に活きてきます。</p>
+    <p class="text">TECH QUESTであれば、より早いペースでコーディングスキル、設計スキルまでを身につけることが可能だと考えています。</p>
+  </div>
+  <div class="imgWrapper">
+    <img src="stephanie-krist-zW4rvExIVcQ-unsplash.jpg" class="img">
+  </div>
+</div>
