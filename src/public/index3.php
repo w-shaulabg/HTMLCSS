96e3af67299268c1aa795c4f1c12957433c1b84b
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-11-10 23:15:57 +0900

    課題３完成版

--- src/public/index3.php
+++ src/public/index3.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="ja">
 <center>
-  <h2>TECH QUESTは</h2>
-  <h2 style="margin-bottom: 20px;">本当にがんばれる人を支えます</h2>
+  <h5 style="margin-bottom: 0px;">TECH QUESTは</h5>
+  <h5 style="margin-bottom: 70px; margin-top: 5px">本当にがんばれる人を支えます</h5>
 </center>
 
-<center>
-  <div center style="display:flex; width: 600px;">
-    <div>
-      <img src="課題3.jpg" class="img" width="300" height="auto">
-    </div>
-    <div style="overflow-wrap: break-word; ">
-      <h2>これからのエンジニア市場</h2>
-      <div style="font-size: 17px">
-        <p style="text-align:left">2022年現在、非常に複雑で高機能なアプリが増えてきています。</p>
-        <p style="text-align:left">そのため「とりあえず動くコード」しか書けないエンジニアは遅かれ早かれ淘汰されていくと感じています。</p>
-        <p style="text-align:left">そんな時代を見据え、私たちTECH QUESTでは「キレイな設計、キレイなコード」を書けるエンジニアを輩出していきたいと考えています。</p>
-      </div>
+
+<div style="display:flex; justify-content: center;">
+  <div>
+    <img src="課題3.jpg" class="img" width="300" height="auto">
+  </div>
+  <div style="margin-left: 2em; max-width:300px;">
+    <h4 style=" margin-top: 0px; text-align: center;">これからのエンジニア市場</h4>
+    <div style="font-size: 10px">
+      <p style="text-align:left">2022年現在、非常に複雑で高機能なアプリが増えてきています。</p>
+      <p style="text-align:left">そのため「とりあえず動くコード」しか書けないエンジニアは遅かれ早かれ淘汰されていくと感じています。</p>
+      <p style="text-align:left">そんな時代を見据え、私たちTECH QUESTでは「キレイな設計、キレイなコード」を書けるエンジニアを輩出していきたいと考えています。</p>
     </div>
   </div>
-</center>
+</div>
